53fda49287d811c599f6aeae06043ee8be45c35f
takahiro okada <t.okada@koshikiad.com>
2021-08-27 04:37:00 +0900

    propsの型定義

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-// eslint-disable-next-line no-use-before-define
-import React from "react";
-import logo from "./logo.svg";
+/* eslint-disable no-use-before-define */
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [text, setText] = useState<string>("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>test</h1>
+      value:{text}
+      <br />
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
     </div>
   );
 }
